9ff74c73b758f7ef7b6a8ac0b4e5d51dc3fcb31b
camila-rocca <camila.rocca@mi.unc.edu.ar>
2026-09-01 13:08:09 -0300

    activacion de pull-up interna de RB0

--- TP2.X/TP2 grupo Transistores.asm
+++ TP2.X/TP2 grupo Transistores.asm
@@ -1,3 +1,9 @@
-s
+
+	
+	;activación de pull-up interna de RB0
+	BANKSEL WPUB
+        BSF WPUB,0       ; habilita resistencia de pull-up para RB0
+        BANKSEL OPTION_REG
+        BCF OPTION_REG,7 ; habilita globalmente pull-ups del PORTB
 
 
