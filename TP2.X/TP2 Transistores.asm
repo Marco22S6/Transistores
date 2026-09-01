5ceb367b9b09ba2cb13367bfba3522509bbe1188
Marco22S6 <marco.scagliotti@mi.unc.edu.ar>
2026-09-01 14:13:26 -0300

    Unificar codigo en archivo principal

--- TP2.X/TP2 Transistores.asm
+++ TP2.X/TP2 Transistores.asm
@@ -17,4 +17,10 @@ _INICIO
 	
 	BANKSEL PORTD
 	CLRF PORTD  ;contador comienza en cero
+	
+	;activación de pull-up interna de RB0
+	BANKSEL WPUB
+        BSF WPUB,0       ; habilita resistencia de pull-up para RB0
+        BANKSEL OPTION_REG
+        BCF OPTION_REG,7 ; habilita globalmente pull-ups del PORTB
 
--- TP2.X/TP2 grupo Transistores.asm
+++
@@ -1,9 +0,0 @@
-
-	
-	;activación de pull-up interna de RB0
-	BANKSEL WPUB
-        BSF WPUB,0       ; habilita resistencia de pull-up para RB0
-        BANKSEL OPTION_REG
-        BCF OPTION_REG,7 ; habilita globalmente pull-ups del PORTB
-
-
